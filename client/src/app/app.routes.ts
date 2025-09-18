34ce8f17994bdd69d44dcba64b1aea48b1290e78
CsalaPeter <csalapeter95@gmail.com>
2025-09-18 14:18:33 +0200

    Add not found page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { NotFound } from './core/errors/not-found/not-found.component';
 
 export const routes: Routes = [
 	{
 		path: "",
 		loadComponent: () => import("./features/games/pages/home/home.component").then(m => m.GamesPageComponent),
-	}
+	},
+	{ path: '**', component: NotFound }
 ];
---
+++ client/src/app/core/errors/not-found/not-found.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	standalone: true,
+	selector: 'not-found',
+	templateUrl: './not-found.component.html',
+	styleUrls: ['./not-found.component.css']
+})
+export class NotFound { }
