30f3dd0c3cfdc70cc7160155a16c038adf4dc245
CsalaPeter <csalapeter95@gmail.com>
2025-09-18 13:42:34 +0200

    Update game card component design

--- client/src/app/features/games/components/game-card/game-card.component.ts
+++ client/src/app/features/games/components/game-card/game-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Game } from '../../models/game.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'app-game-card',
 	templateUrl: './game-card.component.html',
-	styleUrls: ['./game-card.component.css']
+	styleUrls: ['./game-card.component.css'],
+	imports: [CommonModule],
 })
 export class GameCardComponent {
 	@Input() game!: Game
