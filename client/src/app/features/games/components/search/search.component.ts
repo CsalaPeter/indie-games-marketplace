7b861136ac9c3e5a2272b3a8a1c38d45faa326ab
CsalaPeter <csalapeter95@gmail.com>
2025-10-03 11:48:36 +0200

    Finish search dropdown and front end logic

--- client/src/app/features/games/components/search/search.component.ts
+++ client/src/app/features/games/components/search/search.component.ts
@@ -2,14 +2,26 @@ import { Component, inject } from "@angular/core";
 import { GameSearchService } from "../../services/search.service";
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from "@angular/common";
+import { RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-search',
 	templateUrl: './search.component.html',
 	styleUrl: './search.component.css',
-	imports: [FormsModule, CommonModule]
+	imports: [FormsModule, CommonModule, RouterLink]
 })
 
 export class SearchComponent {
 	readonly searchService = inject(GameSearchService)
+
+	hasResults() {
+		if (this.searchService.debouncedSearchTerm().length >= 3) {
+			return true;
+		}
+		return false;
+	}
+
+	selectItem() {
+		this.searchService.updateSearchTerm('');
+	}
 }
