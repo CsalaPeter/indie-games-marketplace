8cde076f350c296ca8c8e546cfc9cee066cd244e
CsalaPeter <csalapeter95@gmail.com>
2025-10-27 11:03:11 +0100

    Add function to clear the selected filters in the list

--- client/src/app/features/filter/components/filter-list/filter-list.component.ts
+++ client/src/app/features/filter/components/filter-list/filter-list.component.ts
@@ -7,7 +7,17 @@ import { Options } from '../../models/option.model';
 	standalone: true,
 	template: `
 		<div class="filter-list">
-			<h4 class="filter-list__title" (click)="isActive = !isActive">{{ title() }}</h4>
+			<div class="filter-list__header">
+				<h4 class="filter-list__header__title" (click)="isActive = !isActive">
+					{{ title() }}
+				</h4>
+				@if (selectedOptions().length !== 0) {
+					<span class="filter-list__header__title-count" (click)="clearFilter()">
+						{{ selectedOptions().length }}
+						<span>X</span>
+					</span>
+				}
+			</div>
 			<div class="filter-list__options" [class.filter-list__options--show]="isActive">
 				@for (option of options(); track option.id) {
 					<filter-option
@@ -27,4 +37,8 @@ export class FilterListComponent {
 	options = input.required<Options[]>();
 	selectedOptions = model.required<string[]>();
 	isActive = false;
+
+	clearFilter() {
+		this.selectedOptions.set([]);
+	}
 }
