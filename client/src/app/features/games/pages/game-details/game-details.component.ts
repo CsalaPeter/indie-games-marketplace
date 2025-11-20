24cfe2d13b8dc784f4bd308f47d3d0efbb0bfa2a
CsalaPeter <csalapeter95@gmail.com>
2025-11-20 11:22:27 +0100

    feat: add Swiper to project

--- client/src/app/features/games/pages/game-details/game-details.component.ts
+++ client/src/app/features/games/pages/game-details/game-details.component.ts
@@ -1,14 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { GameService } from '../../services/games.service';
-import { Game } from "../../models/game.model";
+import { Game } from '../../models/game.model';
 import { HttpResourceRef } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
+import { register } from 'swiper/element/bundle';
+import { Swiper } from 'swiper/types';
+
+register();
 
 @Component({
 	standalone: true,
 	selector: 'app-game-page',
 	templateUrl: './game-details.component.html',
-	styleUrl: './game-details.component.css'
+	styleUrl: './game-details.component.css',
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class GamePage {
 	route = inject(ActivatedRoute);
@@ -17,5 +22,14 @@ export class GamePage {
 
 	constructor(private gameService: GameService) {
 		this.game = this.gameService.getGameResource(this.slug);
+	}
+
+	onProgress(event: CustomEvent<[Swiper, number]>) {
+		const [swiper, progress] = event.detail;
+		console.log(progress);
+	}
+
+	onSlideChange() {
+		console.log('slide changed');
 	}
 }
