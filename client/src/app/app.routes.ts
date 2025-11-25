3b383428ebb41d7ecbec06e8aec7c8e9ef5e610d
CsalaPeter <csalapeter95@gmail.com>
2025-11-25 08:23:45 +0100

    fix(routes): fix GamePage URL after renaming

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { NotFound } from './core/errors/not-found/not-found.component';
 import { GamesPageComponent } from './features/games/pages/home/home.component';
 import { BrowsePageComponent } from './features/games/pages/browse/browse.component';
-import { GamePage } from './features/games/pages/game-details/game-details.component';
+import { GamePage } from './features/games/pages/game-page/game-page.component';
 
 export const routes: Routes = [
 	{
