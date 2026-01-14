23319613f69e021caec26b289fa13a7ea307608d
CsalaPeter <csalapeter95@gmail.com>
2026-01-14 10:16:46 +0100

    feat(client): add uploadGame() service function

--- client/src/app/features/games/services/games.service.ts
+++ client/src/app/features/games/services/games.service.ts
@@ -37,7 +37,20 @@ export class GameService {
 		);
 	}
 
-	uploadGame(formData: FormData) {
+	uploadGame(data: any, coverImage: File, gameFile: File) {
+		const formData = new FormData();
+
+		formData.append('name', data.name);
+		formData.append('slug', data.slug);
+		formData.append('description', data.description);
+		formData.append('price', data.price.toString());
+		formData.append('releaseDate', data.releaseDate);
+		formData.append('tags', JSON.stringify(data.tags));
+		formData.append('platforms', JSON.stringify(data.platforms));
+		formData.append('genres', JSON.stringify(data.genres));
+		formData.append('coverImage', coverImage);
+		formData.append('gameFile', gameFile);
+
 		return this.http.post('/api/upload', formData);
 	}
 }
