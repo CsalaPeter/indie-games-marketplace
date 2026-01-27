efac2178535c515abcc23b4d5029f43e34452804
CsalaPeter <csalapeter95@gmail.com>
2026-01-27 15:37:46 +0100

    fix(client): change css to add responsivnes and moved swiperParams to the .ts file from .html

--- client/src/app/features/home/components/highlight-carousel/highlight-carousel.component.ts
+++ client/src/app/features/home/components/highlight-carousel/highlight-carousel.component.ts
@@ -1,9 +1,17 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
+import {
+	Component,
+	CUSTOM_ELEMENTS_SCHEMA,
+	inject,
+	ViewChild,
+	ElementRef,
+	AfterViewInit,
+} from '@angular/core';
 import { register } from 'swiper/element/bundle';
 import { HighlightService } from '../../services/highlight.service';
 import { HttpResourceRef } from '@angular/common/http';
 import { Highlight } from '../../models/highlight.model';
 import { environment } from '../../../../../environments/environment';
+import { Navigation } from 'swiper/modules';
 
 register();
 
@@ -14,9 +22,27 @@ register();
 	styleUrl: './highlight-carousel.component.css',
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class HighlightCarouselComponent {
+export class HighlightCarouselComponent implements AfterViewInit {
+	@ViewChild('swiperRef') swiperRef!: ElementRef;
 	highlightService = inject(HighlightService);
 	highlights: HttpResourceRef<Highlight[]> = this.highlightService.getHighlights();
+
+	ngAfterViewInit() {
+		const swiperEl = this.swiperRef.nativeElement;
+
+		const swiperParams = {
+			navigation: true,
+			effect: 'fade',
+			spaceBetween: 12,
+			autoplay: {
+				delay: 3500,
+				disableOnInteraction: false,
+			},
+		};
+
+		Object.assign(swiperEl, swiperParams);
+		swiperEl.initialize();
+	}
 
 	getBannerUrl(path: string): string {
 		return `${environment.apiUrl}/${path}`;
